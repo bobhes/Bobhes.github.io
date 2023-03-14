bc889b41e1a6025f978d1852d880f58decbd012e
bobhes <bob_hes@hotmail.com>
2023-03-14 14:36:55 +0100

    k
    k

---
+++ build/precache-manifest.50eca3811a368f1baacd2ceae638ec3d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "fb136497cc45d2543b97",
+    "url": "/static/css/main.3ff61246.chunk.css"
+  },
+  {
+    "revision": "fb136497cc45d2543b97",
+    "url": "/static/js/main.fb136497.chunk.js"
+  },
+  {
+    "revision": "21afc9803ed234d038d2",
+    "url": "/static/css/1.46726ec1.chunk.css"
+  },
+  {
+    "revision": "21afc9803ed234d038d2",
+    "url": "/static/js/1.21afc980.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "51d6d14d7461f33dddcf60de5bb01e2c",
+    "url": "/index.html"
+  }
+];
